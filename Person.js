0b8bc3bd41798251cbba981074d3cf31e598a4b2
brandonborn15 <33734299+brandonborn15@users.noreply.github.com>
2022-08-30 16:08:50 -0500

    edits

---
+++ Person.js
@@ -0,0 +1,8 @@
+const person = {
+    name:'Big Brain',
+    age: 23,
+    departmet:'Information Technology',
+    Supervisor:'Dennis Burnett',
+    ID:01
+}
+module.exports = person;
--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const path = require('path');
+
+const person = require('./Person')
+
+app.use(express.static('html'))
+
+//app.get('/', (req, res) => {
+ // res.send('Hello World!')
+//})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
