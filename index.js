38ba2f9d319471a9309bc6038861b7afc0cc76ee
Thomas Walsh <lwalsh7@luc.edu>
2022-08-30 15:15:58 -0500

    Fat32Club Meeting 3

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.static('basic'))
+
+app.use('/blah', (req, res) => {
+  
 })
 
 app.listen(port, () => {
